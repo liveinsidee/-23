39fb38d921826057a070f9095c9750c8f8be5496
liveinsidee <ruslankapralov91@gmail.com>
2024-01-30 15:34:28 +0600

    homework3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import {useState} from "react";
 import './App.css';
 
 function App() {
+
+  const [tasks, setTask] = useState([])
+  const [inputData, setInputData] = useState('')
+
+  const addPost = () => {
+    setTask([...tasks, inputData])
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" value={inputData} onChange={(event) => {
+        setInputData(event.target.value)
+      }}/>
+      <button onClick={addPost}>Add posts</button>
+
+      <ul>
+        {tasks.map((task) => (<li>{task}</li>))}
+      </ul>
     </div>
   );
 }
